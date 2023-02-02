2ff555b302c5502fc4e3de2b5c43877a4b73cc01
kfinner <16533061+kfinner@users.noreply.github.com>
2023-02-02 11:28:04 -0800

    Add files via upload

--- sis_image_distortion.py
+++ sis_image_distortion.py
@@ -12,29 +12,34 @@ import argparse
 parser = argparse.ArgumentParser(description='Download and drizzle HST observations')
 parser.add_argument("--img", dest="img", action="store", default="none",
                         help="The image that you would like to distort.")
-parser.add_argument("--mass", dest="mass", action="store", default="none",
+parser.add_argument("--mass", dest="mass", action="store", default=5e14,
                         help="The mass of the galaxy or cluster.")
-parser.add_argument("--zsource", dest="zsource", action="store", default="",
+parser.add_argument("--zsource", dest="zsource", action="store", default=0.6,
                         help="The redshift of the source.")
-parser.add_argument("--zlens", dest="zlens", action="store", default="",
+parser.add_argument("--zlens", dest="zlens", action="store", default=0.3,
                         help="The redshift of the lens.")
 parser.add_argument("--pixscale", dest="pixscale", action="store", default=0.1,
                         help="The pixel scale of the image in arcsecs per pixel.")
 args = parser.parse_args()
 
-input_img = Image.open('/Users/kyle/Work/NIRWL/CANDELS/mass_maps/GN/GN_color_cropped.jpeg')
-input_img = np.array(input_img).astype(float)
-input_img = input_img.T
-input_img = input_img[0,:,:]
+def gauss2d(A, cx, cy, sigx, sigy, theta, size):
+    xx, yy = np.meshgrid(np.arange(size),np.arange(size))
+    return A * np.exp(-0.5* ( ( ( ( (xx-cx) * np.cos(theta) + (yy-cy) * np.sin(theta) ) / sigx )**2 ) + ( ( ( (xx-cx) * np.sin(theta) - (yy-cy) * np.cos(theta) ) / sigy )**2 ) ) )
+
+#input_img = Image.open('/Users/kyle/Work/NIRWL/CANDELS/mass_maps/GN/GN_color_cropped.jpeg')
+#input_img = np.array(input_img).astype(float)
+#input_img = input_img.T
+#input_img = input_img[0,:,:]
 
 #input_img = fits.open(args.img)[0].data
-import models
-pad_size = max(input_img.shape)//4
-if pad_size % 2 == 1: pad_size+=1
-input_img = np.pad(input_img, pad_size)
+#import models
+#pad_size = max(input_img.shape)//4
+#if pad_size % 2 == 1: pad_size+=1
+#input_img = np.pad(input_img, pad_size)
 
 for i in range(0,500,5):
-    input_img = models.Gaussian(0, 100, 1000+i, 1000, 3, 3, 0).get_postage(2001)
+    input_img = gauss2d(1, 1000+i, 1000, 3, 3, 0, 2001)
+#models.Gaussian(0, 100, 1000+i, 1000, 3, 3, 0).get_postage(2001)
 
     sigma_v = ( 100 * np.pi * const.G.to('Mpc^3 / (M_sun s^2)')**3 * (args.mass*u.solMass)**2 * cosmo.critical_density(args.zlens).to('M_sun/Mpc^3') / 3 )**(1./6.)
     sigma_v = sigma_v.to('km/s')
@@ -86,8 +91,8 @@ for i in range(0,500,5):
     plt.imshow(theta_img, origin='lower')
     plt.axvline(theta_img.shape[1]//2, color='black')
     plt.axhline(theta_img.shape[0]//2, color='black')
-    plt.xlim(990,1010)
-    plt.ylim(990,1010)
+    plt.xlim(800,1200)
+    plt.ylim(800,1200)
     print(i)
     plt.pause(0.1)
     #plt.close('all')
